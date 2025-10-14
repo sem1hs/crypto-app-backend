dae231aacaff1fcc1c85c8b47146c2fa7b00fae2
Semih Şahinoğlu <semihshngl@gmail.com>
2025-10-14 11:43:38 +0300

    ufak degisiklikler eklendi

--- src/main/java/com/semihsahinoglu/crypto_app/config/WebClientConfig.java
+++ src/main/java/com/semihsahinoglu/crypto_app/config/WebClientConfig.java
@@ -2,15 +2,22 @@ package com.semihsahinoglu.crypto_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient webClient(){
+    public WebClient webClient() {
+        ExchangeFilterFunction logFilter = (request, next) -> {
+            System.out.println("Request: " + request.url());
+            return next.exchange(request).doOnNext(response -> System.out.println("Response status: " + response.statusCode()));
+        };
+
         return WebClient.builder()
                 .baseUrl("https://api.binance.com")
+                .filter(logFilter)
                 .build();
     }
 }
